6c3243029421c4f97addf24353f57fc09470c2c5
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-05-14 15:58:31 +0200

    finito

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landin page</title>
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
+    <title>Landing page</title>
+    <!-- {{asset('/css/app.css')}} -->
 </head>
 <body>
     <div class="container">
-        hello world
+        <h1>Hello World</h1>
     </div>
 </body>
 </html>
